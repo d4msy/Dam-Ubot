c4a695038ef05e547651036ba9ea84231eb776c1
ᴅαᴍ'ѕ‽ <104802621+damsyx@users.noreply.github.com>
2022-09-16 17:53:34 +0700

    Update __main__.py

--- Dam/__main__.py
+++ Dam/__main__.py
@@ -16,9 +16,11 @@ from Dam import BOTLOG_CHATID, LOGGER, LOOP, aiosession, bot1, bots
 from Dam.helpers.misc import create_botlog, git, heroku
 
 MSG_ON = """
-☑️ **Dam-Ubot Activated.**
+☑️ **PyroDam-Userbot Berhasil Di Aktifkan!.**
+━━
 -≽ **🤖 Userbot Version -** `{}`
--≽  **Ketik** `.alive` **untuk Mengecheck Bot**
+-≽ **Ketik** `.alive` **Untuk Mengecheck Bot.**
+━━
 """
 
 
